2a9299159888e33ce55fa41c6b4da7f509c11a49
Anna Tsykalova <tsyan@users.noreply.github.com>
2014-11-07 11:55:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
 $(document).ready(function() {
   $.ajax({
     url: 'http://ig-hacker-news.herokuapp.com/users',
+    // this url comes from rake routes, make sure rake routes says this url accepts a POST request
+    // also make sure that the controller action for this url exists and saves to the database correctly
     type: 'POST',
-    data: {user: {name: 'bobface', about: 'human', email: 'bob@bob.com'}},
-    dataType: 'JSON'
+    // for a GET request you don't have to specify the type of request, because AJAX defaults to GET
+    // for a POST request you have to specify the type of request manually
+    data: {user: {name: 'bobface', about: 'human', email: 'bob@bob.com'}}, 
+    // the data object contains all the info you are sending to the Rails API
+    // the attributes have to match your database schema
+    // by default, you need to specify the model manually - in this case, user
+    // so {user: {name, about, email} } instead of just {name, about, email}
+    // make sure that in your controller, you have params set to allow this info (aka 'strong params')
+    // usually this is a private method with params.require(:user).permit(:name, :about, :email)
+    contentType: 'application/json',
+    // the contentType is important for CORS (cross-origin resource sharing) to work
+    // instead of specifying this for every single AJAX request, you can specify it globally:
+    // $.ajaxSetup({
+    //    contentType: 'application/json'
+    // });
+    // also make sure that in your Rails API Gemfile you have the following line:
+    // gem 'rack-cors', :require => 'rack/cors'
+    // and in your application.rb you have the following code: 
+    // config.middleware.use Rack::Cors do
+    //  allow do
+    //    origins '*'
+    //    resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+    //  end
+    // end
+    dataType: 'JSON' 
+    // dataType here refers to what is being RETURNED from the POST request, not what's being sent
   })
   .done(function(results) {
-    console.log("here is the user I just created, click on the triangle to expand")
+    console.log("here is the user I just created, click on the triangle to expand");
     console.table(results);
+    // do other stuff with the user you just created here
+    // to do that, generally you would call functions that are defined elsewhere in your Javascript file
+    // and either pass in 'results' as the argument to those functions
+    // or assign 'results' to a property of an existing Javascript object
   });
 
   $.ajax({
